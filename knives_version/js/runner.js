d5ad4e7105f77813184686e4f1d974f053eea500
Bryan Eastes <bryan.eastes@gmail.com>
2015-12-12 00:51:05 -0800

    KVINES

---
+++ knives_version/js/runner.js
@@ -0,0 +1,95 @@
+window.addEventListener("load",function() {
+
+var Q = window.Q = Quintus()
+        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
+        .setup({ maximize: true })
+        .controls().touch()
+
+var SPRITE_BOX = 1;
+
+Q.gravityY = 2000;
+
+Q.Sprite.extend("Player",{
+
+  init: function(p) {
+
+    this._super(p,{
+      sheet: "player",
+      sprite: "player",
+      collisionMask: SPRITE_BOX, 
+      x: 40,
+      y: 200,
+      standingPoints: [ [ -16, 44], [ -23, 35 ], [-23,-48], [23,-48], [23, 35 ], [ 16, 44 ]],
+      duckingPoints : [ [ -16, 44], [ -23, 35 ], [-23,-10], [23,-10], [23, 35 ], [ 16, 44 ]],
+      speed: 100,
+      jump: -600
+    });
+
+    this.p.points = this.p.standingPoints;
+
+    this.add("2d, animation");
+  },
+
+  step: function(dt) {
+    this.p.vx += (this.p.speed - this.p.vx)/4;
+
+    if(this.p.y > 555) {
+      this.p.y = 555;
+      this.p.landed = 1;
+      this.p.vy = 0;
+    } else {
+      this.p.landed = 0;
+    }
+
+    if(Q.inputs['up']){
+      this.p.vy = this.p.jump;
+    } 
+
+    this.p.points = this.p.standingPoints;
+    if(this.p.landed) {
+      if(Q.inputs['down']) { 
+        this.play("duck_right");
+        this.p.points = this.p.duckingPoints;
+      } else {
+        this.play("walk_right");
+      }
+    } else {
+      this.play("jump_right");
+    }
+
+    this.stage.viewport.centerOn(this.p.x + 300, 400 );
+
+  }
+});
+
+
+Q.scene("level1",function(stage) {
+
+  stage.insert(new Q.Repeater({ asset: "background-wall.png",
+                                speedX: 0.5 }));
+
+  stage.insert(new Q.Repeater({ asset: "background-floor.png",
+                                repeatY: false,
+                                speedX: 1.0,
+                                y: 300 }));
+
+  stage.insert(new Q.Player());
+  stage.add("viewport");
+
+});
+  
+Q.load("player.json, player.png, background-wall.png, background-floor.png, crates.png, crates.json", function() {
+    Q.compileSheets("player.png","player.json");
+    Q.compileSheets("crates.png","crates.json");
+    Q.animations("player", {
+      walk_right: { frames: [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip: false, loop: true },
+      jump_right: { frames: [13], rate: 1/10, flip: false },
+      stand_right: { frames:[14], rate: 1/10, flip: false },
+      duck_right: { frames: [15], rate: 1/10, flip: false },
+    });
+    Q.stageScene("level1");
+  
+});
+
+
+});
